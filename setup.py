468d4c58faf8714958c4a1c6b13c9309aaa17fca
ibnahmadCoded <alegeaa@yahoo.com>
2021-04-21 22:36:46 +0000

    cookiecutter data science project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='SIT W2D2 Assignment for Data Science Course',
+    author='Aliyu Alege, Ilir Nuredini, Fatmir Nuredini',
+    license='MIT',
+)
